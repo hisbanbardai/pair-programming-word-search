c70095f78dd76ee066243d0030f1adef00c56a12
Hisban Shiraz <hisbanbardai@gmail.com>
2023-09-23 13:56:56 -0600

    Works for horizontal forwards and backwards and verticl forward and backwards

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,64 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  if (!letters.length){
+    return false;
+  }
+
+  const horizontalJoin = letters.map((ls, index) => {
+    return ls.join("");
+  });
+
+  const verticalJoin = verticalLettersArray().map((ls) => {
+    return ls.join("");
+  })
+
+  const backwardsHorizontalJoin = letters.map((ls) => {
+    return ls.reverse().join("");
+  });
+
+  const backwardsVerticalJoin = verticalLettersArray().map((ls) => {
+    return ls.reverse().join("");
+  })
+
+  function verticalLettersArray() {
+    const results = [];
+    for (let i = 0; i < letters[0].length; i++) {
+      const arr = [];
+      for (let j = 0; j < letters.length; j++) {
+        arr.push(letters[j][i]);
+      }
+      results.push(arr);
+    }
+    return results;
+  }
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  for (l of backwardsHorizontalJoin) {
+    //console.log(l);
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  for (l of backwardsVerticalJoin) {
+    //console.log(l);
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
